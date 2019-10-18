7cf1c2698cffa93df82fca0cff850d5d549680e7
louis030195 <louis.beaumont@gmail.com>
2019-10-18 18:12:13 +0300

    Impl GET on connection + fun stuff

--- networks/World_Sync.gd
+++ networks/World_Sync.gd
@@ -1,31 +1,34 @@
 extends HTTPRequest
 
-# TODO: some kind of nice design of a macro level world handler
-# for example it will check if new entities such a new clouds appear ...
-# if there is a new cloud, spawn it then the cloud handle it"s network sync itself ....
+# Currently this script is highly specialized in the handling of object table
+
+export var HOST = "127.0.0.1"
+export var PORT_REST = "8001"
+export var PORT_WEBSOCKET = "3012"
 var ws = null
 var cube = preload("res://scenes/Cube.tscn")
 var instances = Array()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("request_completed", self, "_on_HTTPRequest_request_completed")
 	ws = WebSocketClient.new()
 	ws.connect("connection_established", self, "_connection_established")
 	ws.connect("connection_closed", self, "_connection_closed")
 	ws.connect("connection_error", self, "_connection_error")
 	ws.connect("data_received", self, "_data_received")
 
-	var url = "ws://127.0.0.1:3012"
-	print("Connecting to " + url)
-	print(ws.connect_to_url(url))
-	# $HTTPRequest.request("http://localhost:8001/weather")
-	#pass # Replace with function body.
+	var websocket_url = str("ws://", HOST, ":", PORT_WEBSOCKET)
+	print("Connecting to " + websocket_url)
+	print(ws.connect_to_url(websocket_url))
+	
+	# On connection we want to get all the stuff currently in the db
+	request(str("http://", HOST, ":", PORT_REST, "/objects"))
 
 func _on_HTTPRequest_request_completed( result, response_code, headers, body ):
-	print("Response")
-	var json = JSON.parse(body.get_string_from_utf8())
-	print(json.result)
-	
+	var parsed_json = parse_json(body.get_string_from_utf8())
+	for object in parsed_json:
+		_process_data({"data": object, "protocol": "GET"})
 	
 func _make_post_request(url, data_to_send, use_ssl):
     # Convert data to json string:
@@ -49,36 +52,7 @@ func _data_received():
 	var json_string = ws.get_peer(1).get_packet().get_string_from_ascii()
 	var dict
 	dict = parse_json(json_string)
-	var data = dict["data"]
-	var protocol = dict["protocol"]
-	print("Parsed", dict)
-	print("Data", data)
-	print("Protocol", protocol)
-	var instance
-	#if protocol == "POST":
-
-	#elif protocol == "PUT":
-	for i in instances:
-		if i["data"]["id"] == data["id"]:
-			instance = i["instance"]
-			
-	if not instance:
-		instance = cube.instance()
-		instances.push_front({ "data": data, "instance": instance })
-		var scene_root = get_tree().root.get_children()[0]
-		scene_root.add_child(instance)
-	#else:
-	#	print("Uninplemented protocol:", protocol)
-	#print(instance.get_property_list())
-	#instance.global_transform = self.global_transform
-	#instance.scale = Vector3(data["scale_x"], data["scale_y"], data["scale_z"])
-	instance.translation = Vector3(data["position_x"], data["position_y"], data["position_z"])
-	instance.rotation = Vector3(data["rotation_x"], data["rotation_y"], data["rotation_z"])
-	
-	
-
-	
-	
+	_process_data(dict)
 
 func _process(delta):
 	
@@ -91,3 +65,42 @@ func _process(delta):
 			var test = ws.get_peer(1).get_var()
 			print("recieve %s" % test)
 	"""
+
+# TODO: find better name
+func _process_data(parsed_json):
+	var data = parsed_json["data"]
+	var protocol = parsed_json["protocol"]
+	var instance
+	for i in range(instances.size()):
+		if instances[i]["data"]["id"] == data["id"]:
+			instance = instances[i]["instance"]
+			if protocol == "DELETE":
+				instances.remove(i)
+				instance.queue_free()
+				# return doesn't work in GDScript like other language it seems =)
+		
+	if not instance and protocol != "DELETE":
+		instance = cube.instance()
+		# Random color ^^
+		var color = Color(rand_range(0,255), rand_range(0,255), rand_range(0,255))
+		# TODO: fix it doesn't work different color cuz same object = same material
+		# maybe create new material on runtime ...
+		if data["kind"] == "ground":
+			color = "#663300" # Brown
+		elif data["kind"] == "grass":
+			color = "#003300" # Green
+		instance.get_node("MeshInstance").get_surface_material(0).albedo_color = color
+		#print(instance.get_node("MeshInstance").material_override)
+		instances.push_front({ "data": data, "instance": instance })
+		var scene_root = get_tree().root.get_children()[0]
+		scene_root.add_child(instance)
+		
+	#else:
+	#	print("Uninplemented protocol:", protocol)
+	#print(instance.get_property_list())
+	#instance.global_transform = self.global_transform
+	
+	if instance and protocol != "DELETE":
+		instance.scale = Vector3(data["scale_x"], data["scale_y"], data["scale_z"])
+		instance.translation = Vector3(data["position_x"], data["position_y"], data["position_z"])
+		instance.rotation = Vector3(data["rotation_x"], data["rotation_y"], data["rotation_z"])
